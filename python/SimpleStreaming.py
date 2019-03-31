b9a89dee0a29c07342e5d8a1971c33bb93879fa8
Clayton Shepard <clay@skylarkwireless.com>
2019-03-30 21:30:27 -0500

    Bug fix in SimpleStreaming.py.

--- python/SimpleStreaming.py
+++ python/SimpleStreaming.py
@@ -28,11 +28,11 @@ if __name__ == '__main__':
 	txGain = 40
 	delay = int(10e6)
 	nsamps=8092
-	
+
 	if len(sys.argv) != 2:
 		print("Usage: %s IrisSerial" % os.path.basename(__file__))
 		sys.exit(-1)
-		
+
 	sdr = SoapySDR.Device(dict(driver="iris", serial = sys.argv[1]))
 
 	# Init Rx
@@ -40,44 +40,51 @@ if __name__ == '__main__':
 	sdr.setFrequency(SOAPY_SDR_RX, 0, "RF", freq)
 	sdr.setGain(SOAPY_SDR_RX, 0, rxGain)
 	sdr.setAntenna(SOAPY_SDR_RX, 0, "RX")
-	
+
 	# Init Tx
 	sdr.setSampleRate(SOAPY_SDR_TX, 0, rate)
 	sdr.setFrequency(SOAPY_SDR_TX, 0, "RF", freq)
 	sdr.setGain(SOAPY_SDR_TX, 0, txGain)
 	sdr.setAntenna(SOAPY_SDR_TX, 0, "TRX")
-	
+
 	#Init Buffers
 	sampsRecv = np.zeros(nsamps, dtype=np.complex64)
 	sampsSend = np.zeros(nsamps, dtype=np.complex64)
-	
+
 	#Init Streams
 	rxStream = sdr.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CF32, [0], {})
 	txStream = sdr.setupStream(SOAPY_SDR_TX, SOAPY_SDR_CF32, [0], {})
-	
+
 	ts = sdr.getHardwareTime() + delay
-	sdr.activateStream(txStream, SOAPY_SDR_HAS_TIME, ts)
+	sdr.activateStream(txStream)
 	sdr.activateStream(rxStream, SOAPY_SDR_HAS_TIME, ts)
-		
+
 	t=time.time()
 	total_samps = 0
+
+	#first call to tx needs timestamp
+	sr = sdr.writeStream(txStream, [sampsSend], nsamps, SOAPY_SDR_HAS_TIME, ts+delay)
+	if sr.ret != nsamps:
+		print("Bad Write!!!")
+
 	while True:
-		sr = sdr.writeStream(txStream, [sampsSend], nsamps)
+		#print(sdr.getHardwareTime(),ts + SoapySDR.ticksToTimeNs(total_samps, rate), time.time() - t) #debug
+		sr = sdr.writeStream(txStream, [sampsSend], nsamps, 0) 
 		if sr.ret != nsamps:
 			print("Bad Write!!!")
 		#do some tx processing here
-			
-		sr = sdr.readStream(rxStream, [sampsRecv], nsamps, timeoutUs=int(10e6))		
+
+		sr = sdr.readStream(rxStream, [sampsRecv], nsamps, timeoutUs=int(10e6))	
 		if sr.ret != nsamps:
 			print("Bad Read!!!")
 		#do some rx processing here
-		
+
 		#print an update
 		total_samps += nsamps
 		if time.time() - t > 1:
 			t=time.time()
 			print("Total Samples Sent and Received: %i" % total_samps)
-		
+
 		#It is probably good to sleep here, but the readStream will block sufficiently
 		#it just depends on your processing
 
